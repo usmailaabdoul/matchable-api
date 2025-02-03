ddcd40540902fbc655c80ee602b5067739945ced
usmaila abdoul <ismaelabdul77@gmail.com>
2025-02-03 20:58:32 +0100

    improvements

--- src/main.ts
+++ src/main.ts
@@ -5,7 +5,7 @@ async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors();
 
-  const port = process.env.PORT || 3000;
+  const port = process.env.PORT || 3001;
   await app.listen(port);
 }
 bootstrap();
